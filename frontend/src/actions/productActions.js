64d032b3712c4e1e8674333de95a1daa5c1088f5
rajkamal3 <rkchenumalla@gmail.com>
2020-10-17 23:37:05 +0530

    redux state added to home screen component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,7 @@ import {
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL
 } from './../constants/productConstants';
+import axios from 'axios';
 
 export const listProducts = () => async dispatch => {
     try {
