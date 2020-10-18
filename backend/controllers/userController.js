422f7f151ba3c2c8019997332790d2b3c1de18b1
rajkamal3 <rkchenumalla@gmail.com>
2020-10-18 18:49:20 +0530

    login endpoint added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+import catchAsync from 'express-async-handler';
+import User from './../models/userModel.js';
+
+const authUser = catchAsync(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null
+        });
+    } else {
+        res.status(401);
+        throw new Error(`Invalid email or password.`);
+    }
+});
+
+export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -16,12 +17,14 @@ dotenv.config({
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
@@ -29,7 +32,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`
-    );
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`);
 });
