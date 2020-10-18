351f0b4e44e45d189c5d12dbcfe65ade6683bf6c
rajkamal3 <rkchenumalla@gmail.com>
2020-10-18 20:36:32 +0530

    user registration with password encryption added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,36 @@ const authUser = catchAsync(async (req, res) => {
     }
 });
 
+const registerUser = catchAsync(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error(`User already exists.`);
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(400);
+        throw new Error(`Invalid user data.`);
+    }
+});
+
 const getUserProfile = catchAsync(async (req, res) => {
     const user = await User.findById(req.user._id);
 
@@ -38,4 +68,4 @@ const getUserProfile = catchAsync(async (req, res) => {
     }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
