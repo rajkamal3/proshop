ad7632abdb313e12c92ccf8ffdf153c581b5fb97
rajkamal3 <rkchenumalla@gmail.com>
2020-10-19 23:17:56 +0530

    update user details functionality added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,4 +68,30 @@ const getUserProfile = catchAsync(async (req, res) => {
     }
 });
 
-export { authUser, registerUser, getUserProfile };
+const updateUserProfile = catchAsync(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(201).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        });
+    } else {
+        res.status(404);
+        throw new Error(`User not found.`);
+    }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
