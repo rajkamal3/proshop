66136535107620ada154c28b47341c57f91fbeea
rajkamal3 <rkchenumalla@gmail.com>
2020-10-22 15:17:38 +0530

    get order endpoint added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -25,4 +25,17 @@ const addOrderItems = catchAsync(async (req, res) => {
     }
 });
 
-export { addOrderItems };
+const getOrder = catchAsync(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if (order) {
+        res.json({
+            order
+        });
+    } else {
+        res.status(404);
+        throw new Error(`Order not found.`);
+    }
+});
+
+export { addOrderItems, getOrder };
