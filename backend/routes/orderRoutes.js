dcf171bafd51940923ff5e4e9123201c92868dc8
rajkamal3 <rkchenumalla@gmail.com>
2020-10-23 00:13:56 +0530

    update to paid endpoint added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addOrderItems, getOrder } from './../controllers/orderController.js';
+import { addOrderItems, getOrder, updateOrderToPaid } from './../controllers/orderController.js';
 import { protect } from './../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrder);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
