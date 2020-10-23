6c2b5c1dfbaf55162c9ab7c30cf815b8d69411c5
rajkamal3 <rkchenumalla@gmail.com>
2020-10-23 12:35:03 +0530

    orders and user details reset on logout added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,6 +2,7 @@ import {
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
+    USER_DETAILS_RESET,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
@@ -13,6 +14,7 @@ import {
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL
 } from '../constants/userConstants';
+import { ORDER_LIST_MY_RESET } from './../constants/orderConstants';
 import axios from 'axios';
 
 export const login = (email, password) => async dispatch => {
@@ -46,6 +48,8 @@ export const login = (email, password) => async dispatch => {
 export const logout = () => dispatch => {
     localStorage.removeItem('userInfo');
     dispatch({ type: USER_LOGOUT });
+    dispatch({ type: USER_DETAILS_RESET });
+    dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async dispatch => {
