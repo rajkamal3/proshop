ebbb2e31260f00a01f8c2246a89b66698a43388c
rajkamal3 <rkchenumalla@gmail.com>
2020-10-29 12:17:46 +0530

    meta tag added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import Product from './../components/Product';
 import { listProducts } from './../actions/productActions';
@@ -7,6 +8,7 @@ import Message from './../components/Message';
 import Loader from './../components/Loader';
 import Paginate from './../components/Paginate';
 import ProductCarousel from './../components/ProductCarousel';
+import Meta from './../components/Meta';
 
 const HomeScreen = ({ match }) => {
     const keyword = match.params.keyword;
@@ -25,7 +27,14 @@ const HomeScreen = ({ match }) => {
 
     return (
         <>
-            {!keyword && <ProductCarousel />}
+            <Meta />
+            {!keyword ? (
+                <ProductCarousel />
+            ) : (
+                <Link to="/" className="btn btn-light">
+                    Go back
+                </Link>
+            )}
             <h1>Latest Products</h1>
             {loading ? (
                 <Loader />
